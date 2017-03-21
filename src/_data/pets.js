3e0cbf7221729738dbab66a1f1eb31cab4864971
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-20 18:23:19 -0700

    adds data for pet stores and pets

---
+++ src/_data/pets.js
@@ -0,0 +1,16 @@
+const pets = [
+  {
+    type: 'Dog', 
+    cost: '$2.00'
+  },
+  {
+    type: 'Cat', 
+    cost: '$1.00'
+  },
+  {
+    type: 'Bird', 
+    cost: '$300.00'
+  },
+];
+
+export default pets;
