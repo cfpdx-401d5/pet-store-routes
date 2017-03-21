649236e83d56799ebbb4b671c9184fdeb6a3c490
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-21 11:26:21 -0700

    adds state.pets to Route stores/:storeId/pets shows list of all pets on Store page

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,16 +1,26 @@
 import React from 'react';
 
-const Store = ({ match, stores }) => {
+const Store = ({ match, stores, pets }) => {
   let petStore;
+  let animals;
 
   if (match.params.storeId === stores[0].id) petStore = stores[0];
   else if (match.params.storeId === stores[1].id) petStore = stores[1];
   else if (match.params.storeId === stores[2].id) petStore = stores[2];
   
+  animals = pets.map(x => {
+    return (
+      <li>{x.breed} {x.type}</li>
+    );
+  });
+
   return (
     <div>
       <h3>{petStore.name}</h3>
       <p>Location: {petStore.location}</p>
+      <br/>
+      <h4>Pets at our store</h4>
+      <ul>{animals}</ul>
     </div>
   );
 };
--- src/components/Stores.js
+++ src/components/Stores.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import {
-  Route,
-  Link
+    Route,
+    Link
 } from 'react-router-dom';
 
 const Stores = ({ match, stores }) => {
-// this can be refactored using map to create <Links> with { stores }
-  const northPortland = stores[0].id;
-  const westWillamette = stores[1].id;
-  const eastWillamette = stores[2].id;
+    // this can be refactored using map to create <Links> with { stores }
+  const northPortland = stores[0];
+  const westWillamette = stores[1];
+  const eastWillamette = stores[2];
 
   return (
-        <div>
-            <h2>Stores</h2>
-            <ul>
+    <div>
+        <h2>Stores</h2>
+        <ul>
             <li>
-                <Link to={`${match.url}/${northPortland}`}>
-                North Portland
+                <Link to={`${match.url}/${northPortland.id}/pets`}>
+                    {northPortland.name}
                 </Link>
             </li>
             <li>
-                <Link to={`${match.url}/${westWillamette}`}>
-                West Willamette
+                <Link to={`${match.url}/${westWillamette.id}`}>
+                    {westWillamette.name}
                 </Link>
             </li>
             <li>
-                <Link to={`${match.url}/${eastWillamette}`}>
-                East Willamette
+                <Link to={`${match.url}/${eastWillamette.id}`}>
+                    {eastWillamette.name}
                 </Link>
             </li>
-            </ul>
+        </ul>
 
-            <Route exact path={match.url} render={() => (
+        <Route exact path={match.url} render={() => (
             <h3>Please select a store location.</h3>
-            )}/>
-        </div>
+        )} />
+    </div>
   );
 };
 
