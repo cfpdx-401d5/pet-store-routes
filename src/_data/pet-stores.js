9f16ca37f1bb978658c191820a9e58903a88b3bd
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-21 10:29:51 -0700

    updates data

--- src/_data/pet-stores.js
+++ src/_data/pet-stores.js
@@ -1,16 +1,16 @@
 const petStores = [
   {
-    id: 1,
+    id: '1',
     name: 'North Portland',
     location: '123 N. Williams Ave.'
   }, 
   {
-    id: 2,
+    id: '2',
     name: 'West Willamette',
     location: '456 W. Burnside St.'
   }, 
   {
-    id: 3,
+    id: '3',
     name: 'East Willamette',
     location: '789 Stark St.'
   }
--- src/_data/pets.js
+++ src/_data/pets.js
@@ -1,16 +1,46 @@
 const pets = [
   {
-    type: 'Dog', 
+    id: '1a',
+    type: 'Dog',
+    breed: 'Goldendoodle', 
     cost: '$2.00'
   },
   {
+    id: '2a',
     type: 'Cat', 
+    breed: 'Siamese',
     cost: '$1.00'
   },
   {
+    id: '3a',
     type: 'Bird', 
+    breed: 'Parrot',
     cost: '$300.00'
   },
+  {
+    id: '1ab',
+    type: 'Dog',
+    breed: 'Terrier', 
+    cost: '$2.50'
+  },
+  {
+    id: '2ab',
+    type: 'Cat', 
+    breed: 'Persian',
+    cost: '$1.40'
+  },
+  {
+    id: '3ab',
+    type: 'Bird', 
+    breed: 'Eagle',
+    cost: '$200.00'
+  },
+  {
+    id: '4ab',
+    type: 'Dog',
+    breed: 'Corgi',
+    cost: '$4.00'
+  }
 ];
 
 export default pets;
