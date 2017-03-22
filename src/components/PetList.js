70407abe0850b4bf7a49dc69e07e6f4d6d749acb
cmd-kvn <kvn.m.wong@gmail.com>
2017-03-22 16:30:40 -0700

    changes PetList to class adds select menu that filters by pet type

--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const PetList = ({ match, pets }) => {
-  let petList = pets.map(x => {
-    return (
-      <li key={x.id}>
-        <Link to={`${match.url}/${x.id}`}>
-          {x.breed} {x.type}
-        </Link>
-      </li>
-    );
-  });
+class PetList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      petList: props.pets,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    /* beginning breaking down a query in the url */
+    // console.log('props is', props.location.search.slice(1).split('&').map(x => {
+    //   const new = x.split('=');
+    //   return {  };
+    // }));
+  }
 
-  return (
-    <div>
-      <h4>Pets at our store</h4>
-      <ul>{petList}</ul>
-    </div>
-  );
-};
+  handleChange(e) {
+    if (!e.target.value) {
+      this.setState({ petList: this.props.pets });
+    } else {
+      const filteredPets = this.props.pets.filter(x => {
+        if (!e.target.value) {
+          return true;
+        } else return (x.type === e.target.value);
+      });
+      this.setState({ petList: filteredPets });
+    }
+  }
+
+  render() {
+    const petList = this.state.petList.map(x => {
+      return (
+        <li key={x.id}>
+          <Link to={`${this.props.match.url}/${x.id}`}>
+            {x.breed} {x.type}
+          </Link>
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <h4>Pets at our store</h4>
+        <select onChange={this.handleChange}>
+          <option value='' disabled selected>Sort by pet type</option>
+          <option value=''>All</option>
+          <option value='Dog'>Dog</option>
+          <option value='Cat'>Cat</option>
+          <option value='Bird'>Bird</option>
+        </select>
+        <ul>{petList}</ul>
+      </div>
+    );
+  }
+}
 
 export default PetList;
