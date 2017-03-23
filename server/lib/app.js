47089efc8651f314007c42c0b54ed8cc27ad7891
Brigitte Huneke <brigittehuneke@gmail.com>
2017-03-23 12:02:45 -0700

    started on auth routes

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,12 +1,15 @@
 const app = require('express')();
 const stores = require('./routes/stores');
 const pets = require('./routes/pets');
+const auth = require('./routes/auth');
+//const ensureAuth = require('./auth/ensure-auth');
 const morgan = require('morgan')('dev');
 const cors = require('cors')();
 
 app.use(cors);
 app.use(morgan);
 
+app.use('/auth', auth);
 app.use('/stores', stores);
 app.use('/pets', pets);
 
---
+++ server/lib/routes/auth.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const bodyParser = require('body-parser').json();
+const User = require('../models/user');
+const token = require('../auth/token');
+const ensureAuth = require('../auth/ensure-auth')();
+
+function hasUsernameAndPassword(req, res, next) {
+  const user = req.body;
+  if(!user.username || !user.password) {
+    return next({
+      code: 400,
+      error: 'username and password are required'
+    });
+  }
+  next();
+};
+
+router
+  .get('/verify', ensureAuth, (req, res) => {
+    res.send({ valid: true });
+  })
+  .post('/signup', 
+    bodyParser, 
+    hasUsernameAndPassword, 
+    (req, res, next) => {
+      const userInfo = req.body;
+
+      User.find({ username: userInfo.username }).count()
+        .then(count => {
+          // if (count > 0)
+          //   throw {
+          //     code: 400,
+          //     error: `username ${userInfo.username} already exists`,
+          //   };
+          console.log('here');
+          return new User(userInfo).save();
+        })
+        .then(user => {
+          console.log('user', user);
+          token.sign(user);
+        })
+        .then(token => res.send({ token }))
+        .catch(next);
+    });
+
+module.exports = router;
