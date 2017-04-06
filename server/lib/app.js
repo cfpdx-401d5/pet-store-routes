927db936c1244f35e93db754c36199483bb62747
meghanroserebecca <meghanroserebecca@gmail.com>
2017-04-06 10:49:59 -0700

    backend auth files created and authorization requirement and signup tested via postman

--- server/lib/app.js
+++ server/lib/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')();
 const petStoreRouter = require('./routes/pet-store');
 const petsRouter = require('./routes/pets');
 const breedRouter = require('./routes/pet-breeds');
+const router = require('./routes/auth');
 
 const ensureAuth = require('./auth/ensure-auth')();
 const errorHandler = require('./error-handler')();
@@ -13,6 +14,7 @@ app.use(morgan);
 
 app.use(cors);
 
+app.use('/auth', router);
 app.use('/pet-store', petStoreRouter);
 app.use('/pets', ensureAuth, petsRouter);
 app.use('/breeds', breedRouter);
