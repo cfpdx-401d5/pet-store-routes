dd761d30d2e416d1c7f516cd1f7bd16df8b2f07f
mizutombo <thomas.shultz52@gmail.com>
2017-04-24 11:05:11 -0700

    adds data files for petz-stores.js & petz.js

---
+++ src/data/petz-stores.js
@@ -0,0 +1,22 @@
+const petzStores = [
+  {
+    storeId: 1,
+    storeName: 'BoZen Petz',
+    storeLocation: 'Beaverton',
+    petSpecialty: 'dogs'
+  },
+  {
+    storeId: 2,
+    storeName: 'Featherz Petz',
+    storeLocation: 'Tigard',
+    petSpecialty: 'birds'   
+  },
+  {
+    storeId: 3,
+    storeName: 'Tiger Petz',
+    storeLocation: 'Lake Oswego',
+    petSpecialty: 'cats'    
+  }
+];
+
+export default petzStores;
